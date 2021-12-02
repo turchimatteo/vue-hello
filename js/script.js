bd64d4ae1ad80a0cf3b487fbc9d5c4728b8b47e7
turchimatteo <turchi12@gmail.com>
2021-12-02 15:24:59 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+BONUS:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. 
+*/
+
+Vue.config.devtools = true;
